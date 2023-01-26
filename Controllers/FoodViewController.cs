7f7c0fe609a8a258abac8788b6b55dc59452aa05
CBush88 <christopherbush88@gmail.com>
2023-01-26 17:19:22 -0500

    Added a ViewModel, Controller, and Index/Details Views

---
+++ Controllers/FoodViewController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using RenalTracker.Models;
+using RenalTracker.ViewModel;
+
+namespace RenalTracker.Controllers
+{
+    public class FoodViewController : Controller
+    {
+        private readonly FoodDbContext dbContext = new FoodDbContext();
+
+        public IActionResult Index(int id = 344604, int numResults = 25)
+        {
+            var tables = new FoodViewModel
+            {
+                BrandedFood = dbContext.BrandedFoods.Where(i => i.FdcId >= id && i.FdcId < id+numResults).ToList(),
+                Food = dbContext.Foods.Where(i => i.FdcId >= id && i.FdcId < id+numResults).ToList(),
+                FoodNutrient = null,
+                Nutrient = null
+                //FoodNutrient = dbContext.FoodNutrients.Where(i => i.FdcId == ).ToList(),
+                //Nutrient = dbContext.Nutrients.Where().ToList()
+            };
+
+            //return View(tables);
+            return tables != null ?
+                View(tables) :
+                Problem("Entity set 'tables' is null.");
+        }
+        public IActionResult Details(int? id)
+        {
+            if(id == null || dbContext == null)
+            {
+                return NotFound();
+            }
+            var table = new FoodViewModel();
+            table.BrandedFood = dbContext.BrandedFoods.Where(i => i.FdcId == id).ToList();
+            table.Food = dbContext.Foods.Where(i => i.FdcId == id).ToList();
+            table.FoodNutrient = dbContext.FoodNutrients.Where(i => i.FdcId == id);
+            table.Nutrient = dbContext.Nutrients;
+
+            return View(table);
+        }
+    }
+}
---
+++ ViewModel/FoodViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RenalTracker.Models;
+
+namespace RenalTracker.ViewModel
+{
+    public class FoodViewModel
+    {
+        public IEnumerable<Food> Food { get; set; }
+        public IEnumerable<BrandedFood> BrandedFood { get; set; }
+        public IEnumerable<FoodNutrient>? FoodNutrient { get; set; }
+        public IEnumerable<Nutrient>? Nutrient { get; set; }
+    }
+}
